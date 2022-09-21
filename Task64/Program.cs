c5b3e2c2caac6eca961b831388b1e23b7993c142
Yar_Sanych_NB <y.krivoff@gmail.com>
2022-09-21 15:25:23 +0300

    Add Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 64: Задайте значения M и N. Напишите программу, которая выведет
+все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""
+*/
+
+void PrintNumber(int m, int n)
+{
+  if (n > m) PrintNumber(m, n-1);
+  else  if (m > n) PrintNumber(m, n+1);
+  
+  Console.Write(n + " ");
+}
+
+
+Console.Clear();
+Console.WriteLine($"Для вывода всех натуральных чисел в промежутке от M до N");
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+PrintNumber(m, n);
+
+
+
+
+
+
+
+
+
+
